f6b7f36e91971e4e5f3c6b6443acb21cf0ac1462
krasi <krasimir.xristov@gmail.com>
2023-07-28 21:13:02 +0200

    make my small application reacdy

--- far-away/src/App.js
+++ far-away/src/App.js
@@ -1,3 +1,47 @@
+import { useState } from 'react';
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+];
+
+
+
 export default function App() {
-  return <div>Hello React </div>;
+
+
+  let [steps, setSteps] = useState(1);
+
+  function handlePrevious() {
+
+    if (steps > 1) {
+      setSteps(steps - 1);
+    }
+
+
+  };
+
+  function handleNext() {
+
+    if (steps < 3) {
+      setSteps(steps + 1);
+    }
+
+  }
+
+  return <div className='steps'>
+    <div className='numbers'>
+      <div className={`${steps >= 1 ? 'active' : ''}`}>1</div>
+      <div className={`${steps >= 2 ? 'active' : ''}`}>2</div>
+      <div className={`${steps >= 3 ? 'active' : ''}`}>3</div>
+    </div>
+
+    <p className='message'>Step {steps}: {messages[steps - 1]}</p>
+
+    <div className='buttons' >
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handlePrevious} >Previous</button>
+      <button style={{ backgroundColor: '#7950f2', color: '#fff' }} onClick={handleNext} >Next</button>
+    </div>
+  </div>;
 }
